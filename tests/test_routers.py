7653c3ce92c56cc5faa77e346f677d83fbcfaf3c
user <user@localhost.localdomain>
2026-03-26 22:37:45 +0700

    fix typo

--- tests/test_routers.py
+++ tests/test_routers.py
@@ -1,6 +1,5 @@
 class TestCreateNote:
     async def test_success(self, http):
-        # async with http as http:
         response = await http.post('/', json={
             'name': 'New Note',
             'desc': 'New desc'
@@ -14,25 +13,21 @@ class TestCreateNote:
 
 class TestGetByID:
     async def test_empty(self, http):
-        # async with http as http:
         response = await http.get('/100')
         assert response.status_code == 404
 
     async def test_success(self, http, a_note):
-        # async with http as http:
         response = await http.get(f'/{a_note.id}')
         assert response.status_code == 200
 
 
 class TestGetList:
     async def test_empty(self, http):
-        # async with http as http:
         response = await http.get('/')
         assert response.status_code == 200
         assert len(response.json()) == 0
 
     async def test_success(self, http, a_note):
-        # async with http as http:
         response = await http.get('/')
         assert response.status_code == 200
         assert len(response.json()) > 0
@@ -66,4 +61,3 @@ class TestDelete:
     async def test_not_found(self, http, a_note):
         response = await http.delete('/100')
         assert response.status_code == 404
-
